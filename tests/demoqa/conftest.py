f5b964bc6c00623e35880177869f5734be6b1fe8
sharov_vitalii <fly44088@gmail.com>
2023-11-17 02:15:54 +0300

    + allure step in conftest

--- tests/demoqa/conftest.py
+++ tests/demoqa/conftest.py
@@ -1,10 +1,12 @@
 import pytest
 from selene import browser
+import allure
 
+@allure.title('Open start page')
 @pytest.fixture(scope="function", autouse=True)
 def browser_setup():
-    browser.config.window_width = 1080
-    browser.config.window_height = 1080
+    browser.config.window_width = 1530
+    browser.config.window_height = 900
     browser.config.base_url = 'https://demoqa.com'
 
     yield
