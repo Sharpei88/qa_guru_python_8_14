c54b9637e1bba47a6dd51febc2f11f51a10317a3
sharov_vitalii <fly44088@gmail.com>
2023-11-18 03:47:30 +0300

    add selenoid

--- tests/demoqa/test_registration_form.py
+++ tests/demoqa/test_registration_form.py
@@ -1,12 +1,30 @@
 import os
 import allure
-from selene import browser, have, be
+from selene import have, be
+from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from utils import attach
 
 
 @allure.title('Successful fill form')
 def test_fill_registration_form():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
 
     with allure.step('Open registration form'):
         browser.config.window_width = 1530
